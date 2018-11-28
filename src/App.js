8c70b1cc6eb16b0b38faf8e30d176f25cbc1f752
Lucas Dramowicz <lucas.dramovicz@desjardins.com>
2018-11-28 04:02:28 -0500

    Titles showing on page

--- src/App.js
+++ src/App.js
@@ -9,7 +9,7 @@ const defaultAgeMax = 90;
 const defaultGender = "any";
 
 // tmpSearchObj stores user's preferences before Filter button is clicked
-let tmpSearchObj = [];
+let stories2 = [];
 let warning = '';
 
 
@@ -19,6 +19,7 @@ class App extends React.Component {
 
         this.state = {
             topStoriesIDs: [],
+            stories: [],
             areStoriesLoading: false,
             error: false,
             searchObj: {
@@ -35,24 +36,37 @@ class App extends React.Component {
             showWarning: false,
         };
         // Handler bindings
-        this.onInputChange = this.onInputChange.bind(this);
-        this.onRadioBtnChange = this.onRadioBtnChange.bind(this);
-        this.onResetBtnClick = this.onResetBtnClick.bind(this);
-        this.onFilterBtnClick = this.onFilterBtnClick.bind(this);
-        this.onToggleContactClick = this.onToggleContactClick.bind(this);
     }
     componentDidMount() {
         this.setState({areStoriesLoading: true});
         // fetch + API call
         fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
             .then(results => results.json())
-            .then(data => this.setState({topStoriesIDs: data.slice(0,numOfTopStoriesToFetch), areStoriesLoading: false}))
+            .then(data => {
+                this.setState({topStoriesIDs: data.slice(0,numOfTopStoriesToFetch)})
+                //console.log("data = " + data.slice(0,numOfTopStoriesToFetch))
+            })
+            .then(nextData => {
+                let items = this.state.topStoriesIDs;
+                items.map(story => fetch('https://hacker-news.firebaseio.com/v0/item/' + story + '.json?print=pretty')
+                    .then(results => results.json())
+                    .then((data) => {
+                        //console.log("item = " + data.title);
+                        stories2.push(data);
+                        console.log("story = " + stories2);
+                    })
+                    .then(() => this.setState({stories: stories2, areStoriesLoading: false}))
+                )
+            })
+
             .catch(error => this.setState({error, areStoriesLoading: false}));
     }
 
     render() {
-        console.log("topStoriesIDs = " + this.state.topStoriesIDs);
-        const {areStoriesLoading, error} = this.state;
+        const {areStoriesLoading, error, topStoriesIDs, stories} = this.state;
+        console.log("topStoriesIDs = " + topStoriesIDs);
+        console.log("stories = " + stories);
+
         // check for fetch errors and display them here
         if (error) {
             return <div className="error alert alert-danger makeVisible">
@@ -67,7 +81,13 @@ class App extends React.Component {
         }
 
         return (<div id="App" className="App">
-            <div id="appTitle"><h2 className="text-center">{this.state.topStoriesIDs}</h2></div>
+            <div id="appTitle">
+                {stories.map(story =>
+                <div key={story.id}>
+                    {story.title}
+                </div>
+                )}
+            </div>
         </div>)
     }
 
@@ -80,56 +100,6 @@ class App extends React.Component {
         return text1;
     }
     //Event Listeners
-    onInputChange(el){
-        let inputName = el.target.id;
-        let inputValue = el.target.value;
-        let searchObjCopy = Object.assign({}, tmpSearchObj);
-        searchObjCopy[inputName] = Number(inputValue);
-        tmpSearchObj = Object.assign({}, searchObjCopy);
-    }
-    onRadioBtnChange(el){
-        let inputName = el.target.name;
-        let inputValue = el.target.value;
-        let searchObjCopy = Object.assign({}, tmpSearchObj);
-        searchObjCopy[inputName] = inputValue;
-        tmpSearchObj = Object.assign({}, searchObjCopy);
-        this.setState({gender: inputValue});
-    }
-    onResetBtnClick(){
-        // clear warnings
-        warning = "";
-        this.setState({showWarning: false});
-        // reset searchObj to default values
-        this.setState({searchObj: this.state.defaultSearchObj});
-        // reset tmpSearchObj to default values
-        tmpSearchObj = Object.assign({}, this.state.defaultSearchObj);
-        // remove values from ageMin and ageMax fields
-        //$('[data-id="ageMin"]').val('');
-        //$('[data-id="ageMax"]').val('');
-    }
-    onFilterBtnClick() {
-        // clear warnings
-        warning = "";
-        this.setState({showWarning: false});
-        // check if user age entry fits within age parameters, show error if needed
-        if (tmpSearchObj.ageMin < defaultAgeMin || tmpSearchObj.ageMax > defaultAgeMax) {
-            warning = "ERROR: Age out of " + defaultAgeMin + " - " + defaultAgeMax + " range.";
-            this.setState({showWarning: true});
-            return;
-        }
-        // check if minAge > maxAge, show error if needed
-        if (tmpSearchObj.ageMin > tmpSearchObj.ageMax) {
-            warning = "ERROR: Minimum age exceeds maximum age.";
-            this.setState({showWarning: true});
-            return;
-        }
-        // user choices are valid, pass them to state
-        this.setState({searchObj: tmpSearchObj});
-    }
-    onToggleContactClick(uid) {
-        // toggle show/hide user contact info
-        let contact = document.getElementById("userDialog"+uid);
-        contact.style.display = ((contact.style.display==='none') ? 'block' : 'none');
-    }
+
 }
 export default App;
